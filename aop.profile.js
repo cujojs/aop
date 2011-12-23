34eb114bc31c05b60b5b291a7e1f8f7080e3c422
Bryan Forbes <bryan@reigndropsfall.net>
2011-12-23 11:30:16 -0600

    Fix tabs and spaces.

--- aop.profile.js
+++ aop.profile.js
@@ -2,19 +2,19 @@ var profile = (function(){
 	var testRE = /^aop\/test\//;
 	return {
 		resourceTags: {
-            test: function(filename, mid){
+			test: function(filename, mid){
 				// Tag test files as such
-                return testRE.test(mid);
-            },
-            amd: function(filename, mid){
+				return testRE.test(mid);
+			},
+			amd: function(filename, mid){
 				// Tag the module as AMD so it doesn't get
 				// wrapped by the Dojo builder
-                return mid == "aop/aop";
-            },
-            copyOnly: function(filename, mid){
+				return mid == "aop/aop";
+			},
+			copyOnly: function(filename, mid){
 				// Don't process package.json
-                return mid == "aop/package.json";
-            }
+				return mid == "aop/package.json";
+			}
 		}
 	};
 })();
